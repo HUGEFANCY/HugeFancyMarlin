cd0be9078f02422c52bf592e5728a23884f01f39
Chris <mail@chriswalter.org>
2020-07-09 19:41:24 +0200

    gnd fake teensy

--- TeensyArduino_Codes/Code_Teensy_Extruder/Kuehlung.ino
+++ TeensyArduino_Codes/Code_Teensy_Extruder/Kuehlung.ino
@@ -1,19 +1,20 @@
 
-
-
 byte pwmPinKuehlung = 30;
+byte fakeGroundPin = 31;
 Metro KuehlungUpdateIntervall = Metro(1000);
 
 void KuehlungPWM_setup()
 {
   pinMode(pwmPinKuehlung, OUTPUT); // Setzt den Pin als output.
+  pinMode(fakeGroundPin, OUTPUT);
+  digitalWrite(fakeGroundPin, LOW);
 }
-
 
 void KuehlungPWM_loop()
 {
   if (KuehlungUpdateIntervall.check() == 1)  // check if the metro has passed its interval
   {
-    analogWrite(pwmPinKuehlung, PwmValuePartCoolingFanMarlin);
+    int PWMvalue = map(PwmValuePartCoolingFanMarlin, 0, 255, 255, 0); // PWM Wert umdrehen
+    analogWrite(pwmPinKuehlung, PWMvalue);
   }
 }
