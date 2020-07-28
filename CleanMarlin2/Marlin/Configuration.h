69cb2007cc56cf25235aed4b6ec808367bdd1d9c
Robin Godwyll <rodwyll@gmail.com>
2020-07-28 18:52:53 +0200

    New steps/mm for Z-axis

--- CleanMarlin2/Marlin/Configuration.h
+++ CleanMarlin2/Marlin/Configuration.h
@@ -732,7 +732,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 20.5, 20.5, 200.83, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 20.5, 20.5, 401.66, 500 }
 
 /**
  * Default Max Feed Rate (mm/s)
