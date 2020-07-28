644a3d4132c76b6c18b93236b596c10b0075ab13
Robin Godwyll <rodwyll@gmail.com>
2020-07-28 19:21:05 +0200

    Accel and Velocity change for Z- Axis
    Already changed locally on the marlin board.
    dont forget the M502 + M500 on next upload

--- CleanMarlin2/Marlin/Configuration.h
+++ CleanMarlin2/Marlin/Configuration.h
@@ -739,7 +739,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 200, 200, 5, 100 }
+#define DEFAULT_MAX_FEEDRATE          { 200, 200, 15, 100 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -752,7 +752,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 80, 80, 50, 100 }
+#define DEFAULT_MAX_ACCELERATION      { 80, 80, 20, 100 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
